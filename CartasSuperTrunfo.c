54b91bfd96f5f4b51d12d09d63dd6b8a4870def3
davicordeiiiro <davi.90cordeiro@gmail.com>
2025-04-15 17:18:11 +0000

    aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,32 +18,44 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-   int populacaoa01, populacaob01, pontosa01, pontosb01;
+   int populacao_a01, populacao_b01, pontos_a01, pontos_b01;
    float area_a01, area_b01,  pib_a01, pib_b01;
+   float percapita_a01, percapita_b01, densidade_a01, densidade_b01;
 
    printf("Digite a população de Belo horizonte-MG: \n");
-   scanf("%d", &populacaoa01);
+   scanf("%d", &populacao_a01);
    printf("Digite o número de pontos turísticos de Belo horizonte-MG: \n");
-   scanf("%d", &pontosa01);
+   scanf("%d", &pontos_a01);
    printf("Digite a área de Belo horizonte-MG: \n");
    scanf("%f", &area_a01);
    printf("Digite o PIB de Belo horizonte-MG: \n");
    scanf("%f", &pib_a01);
+   
+   percapita_a01 = populacao_a01 / pib_a01;
+   densidade_a01 = populacao_a01 / area_a01;
+
    printf("Digite a população de Goiânia-GO: \n");
-   scanf("%d", &populacaob01);
+   scanf("%d", &populacao_b01);
    printf("Digite o número de pontos turísticos de Goiânia-GO: \n");
-   scanf("%d", &pontosb01);
+   scanf("%d", &pontos_b01);
    printf("Digite a área de Goiânia-GO: \n");
    scanf("%f", &area_b01);
    printf("Digite o PIB de Goiânia-GO: \n");
    scanf("%f", &pib_b01);
 
+   percapita_b01 = populacao_b01 / pib_b01;
+   densidade_b01 = populacao_b01 / area_b01;
+   
    printf("Belo Horizonte-MG \n");
    printf("código: a01 \n");
-   printf("população: %d \npontos turísticos: %d \nárea: %f \nPIB: %f \n\n", populacaoa01, pontosa01, area_a01, pib_a01);
+   printf("população: %d \npontos turísticos: %d \nárea: %f \nPIB: %f \n", populacao_a01, pontos_a01, area_a01, pib_a01);
+   printf("PIB Per Capita: %.2f \n", percapita_a01);
+   printf("Densidade populacional: %.2f \n\n", densidade_a01);
+   
    printf("Goiânia-GO: \n");
    printf("código: b01 \n");
-   printf("população: %d \npontos turísticos: %d \nárea: %f \nPIB: %f \n", populacaob01, pontosb01, area_b01, pib_b01);
-   
+   printf("população: %d \npontos turísticos: %d \nárea: %f \nPIB: %f \n", populacao_b01, pontos_b01, area_b01, pib_b01);
+   printf("PIB Per Capita: %.2f \n", percapita_b01);
+   printf("Densidade populacional: %.2f \n", densidade_b01);
     return 0;
 }
